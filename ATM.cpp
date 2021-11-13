ce5592f7bf9c5b7823549396799252a177a460b4
Itskrish01 <72066631+Itskrish01@users.noreply.github.com>
2021-11-13 18:42:11 +0530

    Update ATM.cpp

--- ATM.cpp
+++ ATM.cpp
@@ -1,6 +1,6 @@
 /*
 Made by itsKrish01
-If you find any problem in it, then send me the issue at Krishtasood@gmail.com
+If you find any problem in it, then tell me the issue in issue section.
 */
 
 
